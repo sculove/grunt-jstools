0681d12313e888571afd71064f2349d5c36526ce
sculove <sculove@gmail.com>
2013-01-18 18:05:49 +0900

    jstools 플러그인 화일작성

---
+++ tasks/jstools.js
@@ -0,0 +1,7 @@
+module.exports = function(grunt) {
+	grunt.registerMultiTask("jstools", "javascript 프럴그인", function() {
+		var property = this.data;
+		grunt.log.writeln(console.log(data));
+		return;
+	});
+};
